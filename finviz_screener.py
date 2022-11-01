b197e5f330a1b472cd6b6b6dd3a747f32e90863e
atreyasinha <atreyavis@gmail.com>
2022-11-01 09:24:55 -0700

    change parameters

--- finviz_screener.py
+++ finviz_screener.py
@@ -2,9 +2,10 @@ from finviz.screener import Screener
 import random
 
 # screening attributes 
-STOCK_SCREENER = ['cap_midover', 'fa_debteq_low', 'fa_eps5years_pos', 'fa_estltgrowth_pos', 'fa_ltdebteq_u0.5',
-                    'fa_opermargin_pos', 'fa_pe_u25', 'fa_pfcf_u20', 'fa_roa_pos', 'fa_roe_pos', 'fa_roi_o10', 
-                    'fa_sales5years_pos', 'sh_instown_o60', 'ta_sma200_pb']
+STOCK_SCREENER = ['cap_midover', 'fa_debteq_u0.5', 'fa_eps5years_pos', 'fa_ltdebteq_u0.5', 
+                    'fa_netmargin_pos', 'fa_opermargin_pos', 'fa_pe_low', 'fa_pfcf_u20', 
+                    'fa_roa_pos', 'fa_roe_pos', 'fa_roi_o15', 'fa_sales5years_pos', 'sh_avgvol_o400', 
+                    'sh_instown_o50', 'ta_rsi_nob60', 'ta_sma200_pb', 'targetprice_a20']
 
 # error handling for no result
 try:
