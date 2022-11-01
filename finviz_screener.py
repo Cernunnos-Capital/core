29283efb94de3a91749f44bd83045b171d1ecd22
atreyasinha <atreyavis@gmail.com>
2022-11-01 08:14:26 -0700

    remove screening attributes as env

--- finviz_screener.py
+++ finviz_screener.py
@@ -1,9 +1,10 @@
-import os
 from finviz.screener import Screener
 import random
 
 # screening attributes 
-STOCK_SCREENER = os.environ['STOCK_SCREENER']
+STOCK_SCREENER = ['cap_midover', 'fa_debteq_low', 'fa_eps5years_pos', 'fa_estltgrowth_pos', 'fa_ltdebteq_u0.5',
+                    'fa_opermargin_pos', 'fa_pe_u25', 'fa_pfcf_u20', 'fa_roa_pos', 'fa_roe_pos', 'fa_roi_o10', 
+                    'fa_sales5years_pos', 'sh_instown_o60', 'ta_sma200_pb']
 
 # error handling for no result
 try:
