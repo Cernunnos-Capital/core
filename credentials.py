7c5c64f68cb8774648d093ad038b1c12429fc18f
atreyasinha <atreyavis@gmail.com>
2022-11-02 23:38:45 -0700

    cancel pending orders before requests

--- credentials.py
+++ credentials.py
@@ -8,3 +8,6 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # alpaca API
 trading_client = TradingClient.REST(API_KEY, SECRET_KEY, BASE_URL, api_version='v2')
+
+# cancel all pending orders
+trading_client.cancel_all_orders()
