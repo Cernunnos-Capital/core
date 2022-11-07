f42443800e69adaccf7ab9ca928e9838d2cce467
atreyasinha <atreyavis@gmail.com>
2022-11-06 22:05:36 -0800

    create watchlists

--- modules/liquidate.py
+++ modules/liquidate.py
@@ -6,8 +6,9 @@ from credentials import trading_client
 # high valuation
 COUNT = 0
 SOLD = False
-for p in trading_client.list_positions():
-    p_fundamentals = finviz.get_stock(p.symbol)
+get_watchlist = trading_client.get_watchlist_by_name('Long')
+for p in get_watchlist.assets:
+    p_fundamentals = finviz.get_stock(p['symbol'])
     COUNT += 1
 
     if COUNT % 5 == 0:
@@ -18,13 +19,14 @@ for p in trading_client.list_positions():
 
         if ((float(p_fundamentals['PEG']) > 2) and (float(p_fundamentals['P/E']) > 30)
                 and (float(p_fundamentals['P/S']) > 10) and (float(p_fundamentals['P/B']) > 5)):
-            SOLD = True
-            print('Sold', p.symbol)
             trading_client.submit_order(
-                symbol=p.symbol,
+                symbol=p['symbol'],
                 side='sell',
                 qty=p.qty
             )
+            SOLD = True
+            print('Sold', p['symbol'])
+            trading_client.delete_from_watchlist(get_watchlist.id, p['symbol'])
 
 if not SOLD:
     print('All positions are healthy!')
--- modules/order.py
+++ modules/order.py
@@ -12,11 +12,30 @@ account = trading_client.get_account()
 # captial available per stock
 cost_basis = float(os.environ['BUYING_POWER']) / float(len(tickers))
 
-# submit order
-for t in tickers:
-    trading_client.submit_order(
-        symbol=t,
-        side='buy',
-        notional=cost_basis,
-    )
-    print("Bought", t)
+# create watchlists
+watchlists = ['Long']
+for w in watchlists:
+    try:
+        trading_client.create_watchlist(w)
+    except:  # pylint: disable=bare-except
+        pass
+
+
+def submit_order_and_watchlist(watchlist_name):
+    """Submit buy order and add to watchlist"""
+    for sym in tickers:
+        trading_client.submit_order(
+            symbol=sym,
+            side='buy',
+            notional=cost_basis,
+        )
+        print('Bought', sym)
+
+        watchlist_id = trading_client.get_watchlist_by_name(watchlist_name).id
+        try:
+            trading_client.add_to_watchlist(watchlist_id, sym)
+        except:  # pylint: disable=bare-except
+            pass
+
+
+submit_order_and_watchlist('Long')
