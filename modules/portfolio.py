6c1506cd32b8e0ac391c0954c85386150a8a64b7
atreyasinha <atreyavis@gmail.com>
2022-11-13 13:56:31 -0800

    push new code as me

--- modules/portfolio.py
+++ modules/portfolio.py
@@ -21,7 +21,7 @@ intraday_pl = round(intraday_pl, 2)
 
 with open('README.md', 'w', newline='', encoding='utf8') as f:
     f.write('# autoinvest\n')
-    f.write('Automated Investing with Dynamic Rebalancing')
+    f.write('Automated Investing with Dynamic Rebalancing\n')
     f.write('## Portfolio\n')
     f.write('| Cost Basis | Market Value | Unrealized gain |\n')
     f.write('| ------------- |:-------------:| -----:|\n')
