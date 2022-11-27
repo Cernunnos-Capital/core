03f648d1f5135551fda637755b626a0ba6a70b57
atreyasinha <atreyavis@gmail.com>
2022-11-27 00:23:38 -0800

    add news feed and disclosure

--- modules/p_html.py
+++ modules/p_html.py
@@ -1,85 +1,86 @@
+"""Module creates a static webpage"""
 from datetime import date
-from portfolio import data, total_market_value, total_cost_basis, total_pl_pc, intraday_pl, chart
+from portfolio import TOTAL_MARKET_VALUE, TOTAL_COST_BASIS, TOTAL_PL_PC, INTRADAY_PL
+from portfolio import TOP_10_HOLDINGS, CHART, STOCK_NEWS
 
-message = f"""
+COMPANY = 'auto{invest}'
+HTML = f"""
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>autoinvest</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-  </head>
-  <body>
-    <main class="container">
-        <div class="container px-4 py-5">
-        <h2 class="pb-2 border-bottom">auto{{invest}}</h2>
-    
-        <div class="row row-cols-1 row-cols-md-2 align-items-md-center g-5 py-5">
-          <div class="d-flex flex-column align-items-start gap-2">
-            <h3 class="fw-bold" style="color: #272643;">Automated Investing with Dynamic Rebalancing</h3>
-            <p class="text-muted">Actively managed and fundamentally oriented portfolio that identifies and captures compelling long-term investment opportunities</p>
-            <p style="color: #2b0080;">Inception Date: Nov 7, 2022</p>
-            <a href="https://github.com/atreyasinha/autoinvest" target="_blank" class="btn btn-dark btn-lg">Source Code</a>
-          </div>
-          <div class="row row-cols-2 row-cols-sm-2 g-5">
-            <div class="d-flex flex-column gap-2">
-              <h1 class="fw-semibold mb-0" style="color: #ff4500;">${ total_market_value }</h1>
-              <p class="text-muted">Market Value</p>
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>autoinvest</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+   </head>
+   <body>
+      <main class="container">
+         <div class="container px-4 py-5">
+            <h2 class="pb-2 border-bottom">{ COMPANY }</h2>
+            <div class="row row-cols-1 row-cols-md-2 align-items-md-center g-5 py-5">
+               <div class="d-flex flex-column align-items-start gap-2">
+                  <h3 class="fw-bold" style="color: #272643;">Automated Investing with Dynamic Rebalancing</h3>
+                  <p class="text-muted">Actively managed and fundamentally oriented portfolio that identifies and captures compelling long-term investment opportunities</p>
+                  <p class="fw-bold" style="color: #2b0080;">Inception Date: Nov 7, 2022</p>
+                  <a href="https://github.com/atreyasinha/autoinvest" target="_blank" class="btn btn-dark btn-lg">Source Code</a>
+               </div>
+               <div class="row row-cols-2 row-cols-sm-2 g-5">
+                  <div class="d-flex flex-column gap-2">
+                     <h1 class="fw-semibold mb-0" style="color: #ff4500;">${ TOTAL_MARKET_VALUE }</h1>
+                     <p class="text-muted">Market Value</p>
+                  </div>
+                  <div class="d-flex flex-column gap-2">
+                     <h1 class="fw-semibold mb-0">${ TOTAL_COST_BASIS }</h1>
+                     <p class="text-muted">Cost Basis</p>
+                  </div>
+                  <div class="d-flex flex-column gap-2">
+                     <h1 class="fw-semibold mb-0">{ TOTAL_PL_PC }</h1>
+                     <p class="text-muted">Total Returns</p>
+                  </div>
+                  <div class="d-flex flex-column gap-2">
+                     <h1 class="fw-semibold mb-0" style="color: #2c698d;">{ INTRADAY_PL }</h1>
+                     <p class="text-muted">Intraday Returns</p>
+                  </div>
+               </div>
             </div>
-    
-            <div class="d-flex flex-column gap-2">
-              <h1 class="fw-semibold mb-0">${ total_cost_basis }</h1>
-              <p class="text-muted">Cost Basis</p>
+            <embed src="{ CHART }" width="100%" height="500">
+         </div>
+         <h2 class="text-center">Top 10 Holdings</h2>
+         <p class="text-muted text-center">As of { date.today().strftime("%B %d, %Y") }</p>
+         <div class="table-responsive">
+            <table class="table">
+               <thead>
+                  <tr>
+                     <th style="width: 40%;"></th>
+                     <th style="width: 15%;">Weight (%)</th>
+                     <th style="width: 15%;">Avg Entry ($)</th>
+                     <th style="width: 15%;">Today's P/L (%)</th>
+                     <th style="width: 15%;">Total P/L (%)</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  { TOP_10_HOLDINGS }
+               </tbody>
+            </table>
+         </div>
+         <div class="container py-5">
+            <div class="row mb-2">
+               { STOCK_NEWS }
             </div>
-    
-            <div class="d-flex flex-column gap-2">
-              <h1 class="fw-semibold mb-0">{ total_pl_pc }</h1>
-              <p class="text-muted">Total Returns</p>
-            </div>
-    
-            <div class="d-flex flex-column gap-2">
-              <h1 class="fw-semibold mb-0" style="color: #2c698d;">{ intraday_pl }</h1>
-              <p class="text-muted">Intraday Returns</p>
-            </div>
-          </div>
-        </div>
-      </div>
+         </div>
+         <footer class="py-3 my-4 border-top">
+            <p class="text-muted">DISCLOSURE</p>
+            <p><small class="text-muted">All statements made regarding companies, securities or other financial information on this site are strictly beliefs and points of view held by { COMPANY } and are subject to change without notice. Certain information on this site was obtained from sources that { COMPANY } believes to be reliable; however, { COMPANY } does not guarantee the accuracy or completeness of any information obtained from any third party. The information on this site is for informational purposes only and should not be considered as investment advice or a recommendation of any particular security, strategy or investment product. The information on this site is general in nature and should not be considered legal or tax advice. An investor should consult a financial professional, an attorney, or tax professional regarding the investor’s specific situation.</small></p>
 
-      <embed src="{ chart }" width="100%" height="500">
-
-       <br><br>
-      <h2 class="display-6 text-center mb-4">Top 10 Holdings</h2>
-      <p class="text-muted text-center">As of { date.today().strftime("%B %d, %Y") }</p>
-      <div class="table-responsive">
-      <table class="table">
-        <thead>
-          <tr>
-            <th style="width: 40%;"></th>
-            <th style="width: 15%;">Weight (%)</th>
-            <th style="width: 15%;">Avg Entry ($)</th>
-            <th style="width: 15%;">Today's P/L (%)</th>
-            <th style="width: 15%;">Total P/L (%)</th>
-          </tr>
-        </thead>
-
-          
-
-        <tbody>
-          { data }
-        </tbody>
-      </table>
-      </div>
-
-      <footer class="py-3 my-4">
-          <p class="text-center text-muted">© { date.today().strftime("%Y") } autoinvest, Inc</p>
-      </footer>
-    </main>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-  </body>
+            <p><small class="text-muted">Certain hyperlinks or referenced websites on this site may, for your convenience, forward you to third parties' websites, which generally are recognized by their top level domain name. Any descriptions of, references to, or links to other products, publications or services do not constitute an endorsement, authorization, sponsorship or affiliation with { COMPANY } with respect to any linked site or its sponsor, unless expressly stated by { COMPANY }. Any such information, products or sites have not necessarily been reviewed by { COMPANY } and are provided or maintained by third parties over whom { COMPANY } exercises no control. { COMPANY } expressly disclaims any responsibility for the content, the accuracy of the information, and/or the quality of products or services provided by or advertised on these third-party sites. { COMPANY } reserves the right to terminate any hyperlink or hyperlinking program at any time.</small></p>
+            <p class="text-center text-muted">© { date.today().strftime("%Y") } { COMPANY }, Inc</p>
+         </footer>
+      </main>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+   </body>
 </html>
 """
 
 with open('index.html', 'w', newline='', encoding='utf8') as f:
-    f.write(message)
+    f.write(HTML)
     f.close()
--- modules/portfolio.py
+++ modules/portfolio.py
@@ -1,77 +1,130 @@
-from credentials import trading_client
+"""Module generates portfolio information"""
 import finviz
-import time
 import pygal
+from credentials import trading_client
+from pygal.style import DefaultStyle
 
-holdings = []
-total_cost_basis = 0.0
-total_market_value = 0.0
-unrealized_intraday_pl = 0.0
-COUNT = 0
-for position in trading_client.list_positions():
-    stock_data = finviz.get_stock(position.symbol)
-
-    total_cost_basis += float(position.cost_basis)
-    total_market_value += float(position.market_value)
-    unrealized_intraday_pl += float(position.unrealized_intraday_pl)
-
-    holdings.append(
-        [stock_data['Company'], float(position.market_value),
-         round(float(position.current_price), 2),
-         round(float(position.avg_entry_price), 2),
-         str(round(float(position.unrealized_intraday_plpc) * 100, 2)) + '%',
-         str(round(float(position.unrealized_plpc) * 100, 2)) + '%', position.symbol])
-
-    COUNT += 1
-
-    if COUNT % 5 == 0:
-        time.sleep(1)
-
-total_pl_pc = ((total_market_value - total_cost_basis) /
-               total_cost_basis) * 100
-
-total_pl_pc = str(round(total_pl_pc, 2)) + '%'
-total_cost_basis = round(total_cost_basis, 2)
-total_market_value = round(total_market_value, 2)
-
-intraday_pl = (unrealized_intraday_pl / total_market_value) * 100
-intraday_pl = str(round(intraday_pl, 2)) + '%'
+TOTAL_COST_BASIS = 0.0
+TOTAL_MARKET_VALUE = 0.0
+UNREALIZED_INTRADAY_PL = 0.0
+TOTAL_PL_PC = 0.0
+INTRADAY_PL = 0.0
+TOP_10_HOLDINGS = ''
+STOCK_NEWS = ''
+CHART = ''
 
 
-def sort_by_market_value(s):
-    return s[1]
+def get_holdings(TOTAL_COST_BASIS, TOTAL_MARKET_VALUE,
+                 UNREALIZED_INTRADAY_PL, TOTAL_PL_PC, INTRADAY_PL):
+    holdings = []
+    for position in trading_client.list_positions():
+        TOTAL_COST_BASIS += float(position.cost_basis)
+        TOTAL_MARKET_VALUE += float(position.market_value)
+        UNREALIZED_INTRADAY_PL += float(position.unrealized_intraday_pl)
+
+        holdings.append(
+            [trading_client.get_asset(position.symbol).name, float(position.market_value),
+             round(float(position.current_price), 2),
+             round(float(position.avg_entry_price), 2),
+             str(round(float(position.unrealized_intraday_plpc) * 100, 2)) + '%',
+             str(round(float(position.unrealized_plpc) * 100, 2)) + '%', position.symbol])
+
+    TOTAL_PL_PC = ((TOTAL_MARKET_VALUE - TOTAL_COST_BASIS) /
+                   TOTAL_COST_BASIS) * 100
+
+    TOTAL_PL_PC = str(round(TOTAL_PL_PC, 2)) + '%'
+    TOTAL_COST_BASIS = round(TOTAL_COST_BASIS, 2)
+    TOTAL_MARKET_VALUE = round(TOTAL_MARKET_VALUE, 2)
+
+    INTRADAY_PL = (UNREALIZED_INTRADAY_PL / TOTAL_MARKET_VALUE) * 100
+    INTRADAY_PL = str(round(INTRADAY_PL, 2)) + '%'
+
+    return {'HOLDINGS': holdings,
+            'TOTAL_COST_BASIS': TOTAL_COST_BASIS, 'TOTAL_MARKET_VALUE': TOTAL_MARKET_VALUE,
+            'UNREALIZED_INTRADAY_PL': UNREALIZED_INTRADAY_PL, 'TOTAL_PL_PC': TOTAL_PL_PC,
+            'INTRADAY_PL': INTRADAY_PL}
 
 
-data = ""
-count = 0
-sectors = {}
-for stocks in sorted(holdings, key=sort_by_market_value, reverse=True):
-    stock_data = finviz.get_stock(stocks[6])
-    stock_sector = str(stock_data['Sector'])
-    stock_industry = str(stock_data['Industry'])
-    weight = float(round((stocks[1] / total_market_value) * 100, 2))
-    chart_data = {'value': weight, 'label': stocks[0]}
+def sort_by(market_value):
+    """Sorts by market value"""
+    return market_value[1]
 
-    if stock_sector not in sectors.keys():
-        sectors[stock_sector] = [chart_data]
-    else:
-        sectors[stock_sector].append(chart_data)
 
-    if count < 10:
-        data += f"""<tr>
-                <th scope="row" class="text-start" style="color: #272643;">{ stocks[0] }</th>
-                <td>{ str(weight) + '%' }</td>
-                <td class="text-muted">${ stocks[3] }</td>
-                <td style="color: #2c698d;">{ stocks[4] }</td>
-                <td style="color: #2b0080;">{ stocks[5] }</td>
+def populate_table(stocks, weight):
+    return f"""<tr>
+                    <th scope="row" class="text-start" style="color: #272643;">{ stocks[0] }</th>
+                    <td>{ str(weight) + '%' }</td>
+                    <td class="text-muted">${ stocks[3] }</td>
+                    <td style="color: #2c698d;">{ stocks[4] }</td>
+                    <td style="color: #2b0080;">{ stocks[5] }</td>
                 </tr>"""
 
-    count += 1
 
-pie_chart = pygal.Pie(truncate_legend=25)
-pie_chart.title = 'Portfolio slices (in %)'
+def display_news(stocks, n):
+    return f"""<div class="col-md-6">
+                    <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                        <div class="col p-4 d-flex flex-column position-static">
+                            <strong class="d-inline-block mb-2" style="color: #2b0080;">{ n.source }</strong>
+                            <h5 class="mb-0">{ n.headline }</h5>
+                            <div class="mb-1 text-muted">{ stocks[6] }</div>
+                            <a href="{ n.url }" target="_blank" class="stretched-link"></a>
+                        </div>
+                        <div class="col-auto d-none d-lg-block">
+                            <img src="{ n.images[1]['url'] }" width="200" height="180">
+                        </div>
+                    </div>
+                </div>"""
 
-for s in sectors:
-    pie_chart.add(s, sectors[s])
 
-chart = pie_chart.render_data_uri()
+def get_portfolio_attributes(holdings, TOP_10_HOLDINGS, STOCK_NEWS):
+    """Portfolio meta data"""
+    sectors = {}
+    COUNT = 0
+    for stocks in sorted(holdings, key=sort_by, reverse=True):
+        stock_data = finviz.get_stock(stocks[6])
+        STOCK_SECTOR = str(stock_data['Sector'])
+        weight = float(round((stocks[1] / TOTAL_MARKET_VALUE) * 100, 2))
+        chart_data = {'value': weight, 'label': stocks[0]}
+
+        if STOCK_SECTOR not in sectors:
+            sectors[STOCK_SECTOR] = [chart_data]
+        else:
+            sectors[STOCK_SECTOR].append(chart_data)
+
+        if COUNT < 10:
+            TOP_10_HOLDINGS += populate_table(stocks, weight)
+
+            for n in trading_client.get_news(stocks[6], limit=1):
+                if len(n.images) > 0:
+                    STOCK_NEWS += display_news(stocks, n)
+
+        COUNT += 1
+    return {'SECTORS': sectors, 'TOP_10_HOLDINGS': TOP_10_HOLDINGS, 'STOCK_NEWS': STOCK_NEWS}
+
+
+def create_pie_chart(sectors):
+    """Renders pie chart using pygal"""
+    DefaultStyle.background = 'transparent'
+    pie_chart = pygal.Pie(truncate_legend=25, style=DefaultStyle)
+
+    for s in sectors:
+        pie_chart.add(s, sectors[s])
+    return pie_chart.render_data_uri()
+
+
+"""Execution"""
+PORTFOLIO = get_holdings(TOTAL_COST_BASIS, TOTAL_MARKET_VALUE,
+                         UNREALIZED_INTRADAY_PL, TOTAL_PL_PC, INTRADAY_PL)
+
+TOTAL_MARKET_VALUE = PORTFOLIO['TOTAL_MARKET_VALUE']
+DISPLAY_PORTFOLIO_DATA = get_portfolio_attributes(
+    PORTFOLIO['HOLDINGS'], TOP_10_HOLDINGS, STOCK_NEWS)
+
+CHART = create_pie_chart(DISPLAY_PORTFOLIO_DATA['SECTORS'])
+
+TOTAL_COST_BASIS = PORTFOLIO['TOTAL_COST_BASIS']
+UNREALIZED_INTRADAY_PL = PORTFOLIO['UNREALIZED_INTRADAY_PL']
+TOTAL_PL_PC = PORTFOLIO['TOTAL_PL_PC']
+INTRADAY_PL = PORTFOLIO['INTRADAY_PL']
+TOP_10_HOLDINGS = DISPLAY_PORTFOLIO_DATA['TOP_10_HOLDINGS']
+STOCK_NEWS = DISPLAY_PORTFOLIO_DATA['STOCK_NEWS']
