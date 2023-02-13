a19bab704ecfb1c250ef319aab3f546f5c367671
atreyasinha <atreyavis@gmail.com>
2023-02-12 20:33:08 -0800

    add try/catch for fetching ratios

--- modules/liquidate.py
+++ modules/liquidate.py
@@ -4,32 +4,28 @@ import finviz
 from credentials import trading_client
 
 # high valuation
-COUNT = 0
 SOLD = False
 get_watchlist = trading_client.get_watchlist_by_name('Long')
 for p in get_watchlist.assets:
     p_fundamentals = finviz.get_stock(p['symbol'])
 
-    PEG_RATIO = p_fundamentals['PEG']
-    PS_RATIO = p_fundamentals['P/S']
-    PB_RATIO = p_fundamentals['P/B']
-
-    COUNT += 1
-
-    if COUNT % 5 == 0:
+    try:
+        PEG_RATIO = p_fundamentals['PEG']
+        PS_RATIO = p_fundamentals['P/S']
+        PB_RATIO = p_fundamentals['P/B']
+    except:
         time.sleep(1)
+        PEG_RATIO = p_fundamentals['PEG']
+        PS_RATIO = p_fundamentals['P/S']
+        PB_RATIO = p_fundamentals['P/B']
 
     STRIKE = 0
     if ((PEG_RATIO != '-') and (PS_RATIO != '-') and (PB_RATIO != '-')):
-        PEG_RATIO = float(PEG_RATIO)
-        PS_RATIO = float(PS_RATIO)
-        PB_RATIO = float(PB_RATIO)
-
-        if PEG_RATIO > 2:
+        if float(PEG_RATIO) > 2:
             STRIKE += 1
-        if PS_RATIO > 10:
+        if float(PS_RATIO) > 10:
             STRIKE += 1
-        if PB_RATIO > 5:
+        if float(PB_RATIO) > 5:
             STRIKE += 1
 
         if STRIKE > 1:
