b3b4dac1dbf628b9e02d4bfd96740225428def67
atreyasinha <atreyavis@gmail.com>
2023-03-19 14:37:26 -0700

    entity to var

--- modules/screen.py
+++ modules/screen.py
@@ -42,7 +42,8 @@ def trim(data, ins_trn, pe_ratio, pfcf, peg, roe):
         if pe_ratio > 13.87 or roe < 20 or pfcf > 10.77:
             return
 
-    match data['Sector']:
+    sector = data['Sector']
+    match sector:
         case 'Basic Materials':
             if pe_ratio > 4.164 or pfcf > 20.60:
                 return
