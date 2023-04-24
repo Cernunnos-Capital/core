2f1984886c02d9cefffadef906264955b66aef91
atreyasinha <atreyavis@gmail.com>
2023-04-24 09:24:28 -0400

    add pro-rated buying

--- order.py
+++ order.py
@@ -1,4 +1,6 @@
 """Module submits buy order using Alpaca API"""
+import calendar
+from datetime import date
 import os
 from credentials import trading_client
 from screen import tickers
@@ -6,8 +8,16 @@ from screen import tickers
 # account info
 account = trading_client.get_account()
 
+# pro-rated capital
+today = date.today()
+last_day = f'{today.year}-{today.month}-{calendar.monthrange(today.year, today.month)[1]}'
+
+trading_days = 0
+for i in trading_client.get_calendar(start=today, end=last_day):
+    trading_days += 1
+
 # captial available per stock
-cost_basis = float(os.environ['BUYING_POWER']) / float(len(tickers))
+cost_basis = float(trading_client.get_account().cash) / trading_days
 
 # submit buy order
 for sym in tickers:
