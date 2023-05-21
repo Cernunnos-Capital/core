e5acceaddad2a1b31e4c626a93b97f179445237f
atreyasinha <atreyavis@gmail.com>
2023-05-21 13:19:08 -0700

    fix: rebalance, add get_all_positions

--- rebalance.py
+++ rebalance.py
@@ -1,16 +1,26 @@
 """Module rebalances the portfolio by buying underwater equities"""
 import math
 from credentials import trading_client
+from alpaca.trading.requests import MarketOrderRequest
+from alpaca.trading.enums import OrderSide, TimeInForce
 
-for p in trading_client.list_positions():
+for p in trading_client.get_all_positions():
     loss = float(p.unrealized_plpc)
 
     if 0 > loss > -0.75:
         cost_basis = math.ceil(abs(float(p.unrealized_pl)) / 10) * 40
 
-        trading_client.submit_order(
+        # preparing orders
+        market_order_data = MarketOrderRequest(
             symbol=p.symbol,
-            side='buy',
-            notional=cost_basis,
+            side=OrderSide.BUY,
+            time_in_force=TimeInForce.DAY,
+            notional=cost_basis
         )
+
+        # submit buy order
+        trading_client.submit_order(
+            order_data=market_order_data
+        )
+        
         print('Bought', p.symbol)
