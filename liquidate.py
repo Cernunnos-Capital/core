9b3789e51a23d2c670a0bcd0bd17beb1dcdf2deb
atreyasinha <atreyavis@gmail.com>
2023-05-21 18:54:36 -0700

    catch if not fetched

--- liquidate.py
+++ liquidate.py
@@ -5,12 +5,18 @@ from orders import trailing_sell, sell
 
 # high valuation
 SOLD = False
+KEEP_FETCHING = True
 for p in trading_client.get_all_positions():
-    data = fetch_fundamentals(p.symbol)
-    if data is None:
-        continue
+    while KEEP_FETCHING:
+        try:
+            data = fetch_fundamentals(p.symbol)
+            if data is None:
+                continue
+            PE_RATIO = str_perc(data['P/E'])
+        except KeyError:
+            print(p.symbol, ': could not fetch')
+        KEEP_FETCHING = False
 
-    # PE_RATIO = str_perc(data['P/E'])
     FWD_PE_RATIO = str_perc(data['Forward P/E'])
     PEG_RATIO = str_perc(data['PEG'])
     PS_RATIO = str_perc(data['P/S'])
