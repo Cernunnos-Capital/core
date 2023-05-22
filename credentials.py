517d5a2dd598f80892579f175ac81fd0cc33902b
atreyasinha <atreyavis@gmail.com>
2023-05-21 19:26:27 -0700

    update alpaca api

--- credentials.py
+++ credentials.py
@@ -1,6 +1,6 @@
 """Module connects Alpaca API"""
 import os
-# import sys
+import sys
 from alpaca.trading.client import TradingClient
 
 # credentials
@@ -15,6 +15,6 @@ else:
     trading_client = TradingClient(API_KEY, SECRET_KEY, paper=False)
 
 # check market hours
-# if not trading_client.get_clock().is_open:
-#     print('Market is Closed!')
-#     sys.exit()
+if not trading_client.get_clock().is_open:
+    print('Market is Closed!')
+    sys.exit()
