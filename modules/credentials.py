ef0147241b6c8879f3788af642a8d475eaae8894
atreyasinha <atreyavis@gmail.com>
2023-12-22 15:32:27 -0800

    check market hours

--- modules/credentials.py
+++ modules/credentials.py
@@ -1,6 +1,6 @@
 """Module connects Alpaca API"""
 import os
-# import sys
+import sys
 from alpaca.trading.client import TradingClient
 
 # credentials
@@ -14,7 +14,7 @@ if 'paper' in BASE_URL:
 else:
     trading_client = TradingClient(API_KEY, SECRET_KEY, paper=False)
 
-# # check market hours
-# if not trading_client.get_clock().is_open:
-#     print('Market is Closed!')
-#     sys.exit()
+# check market hours
+if not trading_client.get_clock().is_open:
+    print('Market is Closed!')
+    sys.exit()
