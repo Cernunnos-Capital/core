d9453c3236d28b6b17f45bacaa34db9492f846cf
atreyasinha <atreyavis@gmail.com>
2024-03-14 15:27:53 -0700

    test eps

--- modules/liquidate.py
+++ modules/liquidate.py
@@ -11,45 +11,48 @@ for p in trading_client.get_all_positions():
     except:  # pylint: disable=bare-except
         continue
 
-    PE_RATIO = str_perc(data['P/E'])
-    FWD_PE_RATIO = str_perc(data['Forward P/E'])
-    PEG_RATIO = str_perc(data['PEG'])
-    PS_RATIO = str_perc(data['P/S'])
-    PB_RATIO = str_perc(data['P/B'])
-    INSIDER_TRANS = str_perc(data['Insider Trans'])
-    TARGET_PRICE = str_perc(data['Target Price'])
-    CURRENT_PRICE = str_perc(data['Price'])
-    RSI = str_perc(data['RSI (14)'])
+    EPS_GROWTH = str_perc(data['EPS next 5Y'])
+    print(p.symbol, EPS_GROWTH)
 
-    STRIKE = 0
-    if PEG_RATIO > 2:
-        STRIKE += 1
-    if PS_RATIO > 10:
-        STRIKE += 1
-    if PB_RATIO > 5:
-        STRIKE += 1
-    if INSIDER_TRANS < -20:
-        STRIKE += 1
+    # PE_RATIO = str_perc(data['P/E'])
+    # FWD_PE_RATIO = str_perc(data['Forward P/E'])
+    # PEG_RATIO = str_perc(data['PEG'])
+    # PS_RATIO = str_perc(data['P/S'])
+    # PB_RATIO = str_perc(data['P/B'])
+    # INSIDER_TRANS = str_perc(data['Insider Trans'])
+    # TARGET_PRICE = str_perc(data['Target Price'])
+    # CURRENT_PRICE = str_perc(data['Price'])
+    # RSI = str_perc(data['RSI (14)'])
 
-    print(f'({STRIKE}) : ', end='')
-    print(PE_RATIO, FWD_PE_RATIO, PEG_RATIO, PS_RATIO, PB_RATIO,
-          INSIDER_TRANS, TARGET_PRICE, CURRENT_PRICE, RSI)
+    # STRIKE = 0
+    # if PEG_RATIO > 2:
+    #     STRIKE += 1
+    # if PS_RATIO > 10:
+    #     STRIKE += 1
+    # if PB_RATIO > 5:
+    #     STRIKE += 1
+    # if INSIDER_TRANS < -20:
+    #     STRIKE += 1
 
-    if (FWD_PE_RATIO > PE_RATIO) and (STRIKE > 1) and (RSI > 60) and (CURRENT_PRICE > TARGET_PRICE):
-        SOLD = True
-        QTY = float(p.qty)
-        TRAILING_SELL_QTY = int(QTY)
+    # print(f'({STRIKE}) : ', end='')
+    # print(PE_RATIO, FWD_PE_RATIO, PEG_RATIO, PS_RATIO, PB_RATIO,
+    #       INSIDER_TRANS, TARGET_PRICE, CURRENT_PRICE, RSI)
 
-        try:
-            if TRAILING_SELL_QTY > 0:
-                trailing_sell(p.symbol, TRAILING_SELL_QTY)
+    # if (FWD_PE_RATIO > PE_RATIO) and (STRIKE > 1) and (RSI > 60) and (CURRENT_PRICE > TARGET_PRICE):
+    #     SOLD = True
+    #     QTY = float(p.qty)
+    #     TRAILING_SELL_QTY = int(QTY)
 
-            LIQUIDATION_SELL_QTY = QTY - TRAILING_SELL_QTY
+    #     try:
+    #         if TRAILING_SELL_QTY > 0:
+    #             trailing_sell(p.symbol, TRAILING_SELL_QTY)
 
-            sell(p.symbol, LIQUIDATION_SELL_QTY)
-            print('Sold', p.symbol)
-        except:  # pylint: disable=bare-except
-            print(p.symbol, ': Sell order already placed!')
+    #         LIQUIDATION_SELL_QTY = QTY - TRAILING_SELL_QTY
+
+    #         sell(p.symbol, LIQUIDATION_SELL_QTY)
+    #         print('Sold', p.symbol)
+    #     except:  # pylint: disable=bare-except
+    #         print(p.symbol, ': Sell order already placed!')
 
 
 if not SOLD:
